d6e9d2ebec9c02236a695ef3f331fe0d736685e2
Svenb <sven.bouwmeester93@gmail.com>
2024-09-16 12:57:39 +0200

    Moved

---
+++ ESS11/documentation/lists_dic_repository.py
@@ -0,0 +1,71 @@
+dic_cat_missing_thou = {
+    6666: 'not applicable',
+    7777: 'refusal',
+    8888: 'dont know',
+    9999: 'no answer',
+    999: 'not available',
+}
+
+dic_cat_missing_ten = {
+    66: 'not applicable',
+    77: 'refusal',
+    88: 'dont know',
+    99: 'no answer'
+}
+
+dic_cat_missing_single = {
+6: 'not applicable',
+7: 'refusal',
+9: 'no answer',
+}
+
+dic_political_parties = {
+    1: "People's Party for Freedom and Democracy",
+    2: "Labour Party",
+    3: "Party for Freedom",
+    4: "Socialist Party",
+    5: "Christian Democratic Appeal",
+    6: "Democrats '66",
+    7: "Christian Union",
+    8: "Green Left",
+    9: "Reformed Political Party",
+    10: "Party for the Animals",
+    11: "50PLUS",
+    12: "DENK",
+    13: "Forum for Democracy",
+    14: "Artikel1/Bij1",
+    15: "Volt",
+    16: "JA21",
+    17: "BoerBurgerBeweging",
+    18: "Blanc",
+    31: "Other",
+    66: "Not applicable*",
+    77: "Refusal*",
+    88: "Don't know*",
+    99: "No answer*"
+}
+
+dic_education_level_nl = {
+    1: "Basisschool niet afgemaakt",
+    2: "Alleen basisschool afgemaakt",
+    3: "LBO, VBO, LEAO, LTS ambachtsschool, huishoudschool, LHNO, VMBO (niveaus 1-3; basisberoepsgericht, kaderberoepsgericht, g",
+    4: "MULO, ULO, MAVO, VMBO (niveau 4; theoretische leerweg); HAVO jaar 3-4; VWO jaar 3-5 afgemaakt",
+    5: "MBO niveau 1 afgemaakt (duur < 2 jaar)",
+    6: "HAVO, MMS, MSVM afgemaakt",
+    7: "VWO, HBS, atheneum, gymnasium afgemaakt",
+    8: "KMBO, leerlingwezen, MEAO, MTS afgemaakt (duur 2-3 jaar)",
+    9: "MBO niveau 2 en 3 afgemaakt (duur 2-3 jaar)",
+    10: "MBO niveau 4 afgemaakt (duur 4 jaar)",
+    11: "MBO-plus voor havisten",
+    12: "propedeuse WO, OU-certificaat",
+    13: "korte HBO-opleiding einddiploma (2 of 3 jaar)",
+    14: "Bachelor HBO, kweekschool, PABO, conservatorium, MO-akten afgemaakt",
+    15: "Bachelor universiteit afgemaakt",
+    16: "HBO: Master's degree, tweede fase opleidingen; Post HBO-opleidingen, pre-master onderwijs voor HBO",
+    17: "WO/universiteit: Master's degree, tweede fase opleidingen; ingenieur, meester, doctorandus",
+    18: "Doctoraat / gepromoveerd",
+    5555: "Other",
+    7777: "Refusal*",
+    8888: "Don't know*",
+    9999: "No answer*"
+}
