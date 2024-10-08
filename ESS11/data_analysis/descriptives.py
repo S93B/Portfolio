30df5f18220ab50a57a7efb85ca830555a6d0ae8
Sven <sven.bouwmeester93@gmail.com>
2024-10-08 11:19:00 +0200

    Add educational level recoding and visualization
    Introduce a new function to recode educational levels using binning. Also, update data analysis scripts to visualize political trust by educational level and other sociodemographic factors.

--- ESS11/data_analysis/descriptives.py
+++ ESS11/data_analysis/descriptives.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 path = 'C:\Python homedirectory\Portfolio_git\ESS11\data\processed\data_NL_transf_v2.csv'
 df = pd.read_csv(path, index_col=0)
 
@@ -8,20 +9,38 @@ print(df.columns)
 
 #rename pol vars for easy readability
 df.rename(columns={'trstprl': 't_parlement', 'trstlgl': 't_legalsystem', 'trstplc': 't_police', 'trstplt': 't_politicians', 'trstprt': 't_political_parties', 'trstep': 't_EU_parlement', 'trstun': 't_united_nations'}, inplace=True)
-#rename social trust vars
 
-#rename national id vars
+#Create dataframe for analysis political trust
+educational_bins = [0,5,11,18]
+educational_levels = ['lower_educated', 'mid_educated', 'highly_educated']
+df['educational_level'] = pd.cut(df['education'], bins=educational_bins, labels=educational_levels)
+df_pol = df.loc[:, ['age', 'gender', 'education', 'educational_level', 'activity_work', 'voted', 'political_trust']]
 
+# sociodemo descriptivesoo
+df_describe = df_pol.describe()
 
-# sociodemo descriptives
-slice = df.iloc[:, [0,1,2]]
-slice1 = slice.groupby('gender')
-desr_slice1 = slice1.describe()
-
-slice2 = df.groupby(by = ['gender', 'educational_level'])
+slice = df_pol.loc[:, ['political_trust', 'educational_level']]
+slice2 = slice.groupby(by = ['educational_level'])
 desc_slice2 = slice2.describe()
-sns.displot(data=slice1, x='age', kind='hist')
-plt.show()
 
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df_pol, x='educational_level', y='political_trust')
+plt.savefig(fr'C:\Python homedirectory\Portfolio_git\ESS11\reports\figures\political_trust_line.png')
+
+#Exploration plots
+plot_categoricals = ['gender', 'educational_level', 'activity_work', 'voted']
+plot_continous = ['age', 'political_trust']
+
+for feature in plot_categoricals:
+    plt.figure(figsize=(10, 6))
+    sns.countplot(data=df_pol, x=feature,stat='percent')
+    plt.savefig(fr'C:\Python homedirectory\Portfolio_git\ESS11\reports\figures\{feature}.png')
+    plt.close()
+
+for feature in plot_continous:
+    plt.figure(figsize=(10, 6))
+    sns.displot(data=df_pol, x=feature, stat='frequency')
+    plt.savefig(fr'C:\Python homedirectory\Portfolio_git\ESS11\reports\figures\{feature}.png')
+    plt.close()
 
 #Divide per voted political party and compare % to latest election
