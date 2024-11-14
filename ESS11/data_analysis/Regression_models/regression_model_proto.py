a0d6131b9ab1402acdc45020aed9f923a20c4711
Sven <sven.bouwmeester93@gmail.com>
2024-11-14 10:56:34 +0100

    Working branch

--- ESS11/data_analysis/Regression_models/regression_model_proto.py
+++ ESS11/data_analysis/Regression_models/regression_model_proto.py
@@ -6,11 +6,18 @@ from sklearn.metrics import mean_squared_error, r2_score
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-path = r'/ESS11/data/processed/data_NL_transf_kneighbor_v2.csv'
+path = r'C:\Python homedirectory\Portfolio_git\ESS11\data\processed\data_NL_transf_kneighbor_v2.csv'
 df = pd.read_csv(path, index_col=0)
 
 #TODO model uitwerken in mindmap
 
+f, ax = plt.subplots(figsize=(20,14))
+corr = df.corr(numeric_only=True, method='pearson')
+hm = sns.heatmap(round(corr,2), annot=True, ax=ax, cmap="coolwarm",fmt='.2f', vmin=-1, vmax=1, yticklabels=corr.columns, xticklabels=corr.columns)
+f.subplots_adjust(top=0.83)
+t= f.suptitle('Correlation Heatmap', fontsize=14)
+plt.show()
+#plt.savefig(r'C:\Python homedirectory\Portfolio_git\ESS11\reports\figures\correlation_heatmap_all.png')
 
 ########################################## niet gebruiken
 stat, p_value=stats.shapiro(df['political_trust'])
@@ -28,16 +35,16 @@ educational_bins = [0, 5, 11, 18]
 educational_levels = [1,2,3]
 df['educational_level'] = pd.cut(df['education'], bins=educational_bins, labels=educational_levels)
 
-sns.scatterplot(df, y='education', x='political_trust')
-g = sns.jointplot(x="education", y="political_trust", data=df,
+sns.scatterplot(df, x='stfgov', y='political_trust')
+g = sns.jointplot(x="stfgov", y="political_trust", data=df,
                   kind="reg", truncate=False,
                   xlim=(0, 60), ylim=(0, 12),
                   color="m", height=7)
 plt.show()
 
-#MODEL EDU > VERTROUWEN nogo
-y = df[['educational_level']]
-X = df[['political_trust']]
+
+X = df[['stfgov']]
+y = df[['political_trust']]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
